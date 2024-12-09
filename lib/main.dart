1ace051f9dd98a7dcee52bc53688eb61ff472c8e
Kiran Saleem <saleemkiran1599@gmail.com>
2024-12-09 21:58:24 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              color: Colors.deepPurple,
+              padding: EdgeInsets.all(20),
+              child: Text(
+                'HELLO WORLD',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Container(
+              color: Colors.deepPurple,
+              padding: EdgeInsets.all(20),
+              margin: EdgeInsets.all(10),
+              child: Text(
+                'Hi!!!',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Image(
+              image: AssetImage('images/computer.jpg'),
+            ),
+          ],
+        )),
+      ),
+    );
+  }
+}
